74a0a73748d8edc2bb2962765db95b60a45c9393
TGHeadle1371 <tgheadle@gmail.com>
2018-12-03 17:36:39 -0700

    Whatever changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -56,53 +56,53 @@ var bandNames = [ //Bands word list
 // Congratulate the player on guessing the word
 
 
- var game = {
-     guessed: [],
-     left: 10,
-     start: function () {
-         this.complete = false;
-         this.word = bandNames[Math.floor(Math.random() * bandNames.length)];
-         this.$wins = document.getElementById('wins');
-         this.$losses = document.getElementById('losses');
-         this.$remaining = document.getElementById('remaining');
-         this.$lettersGuessed = document.getElementById('lettersGuessed');
-         this.$right.innerHTML = '_'.repeat(this.word.length);
-     },
-     guess: function (letter) {
-         if (this.left > 0 && this.complete != true) {
-             if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
-                 this.wins(letter);
-             } else {
-                 this.losses(letter);
-             }
-         }
-     },
-     wins: function (letter) {
-         for (var i = 0; i < this.word.length; i++) {
-             if (this.word[i] == letter) {
-                 var word = this.$wins.innerHTML.split('');
-                 word[i] = letter;
-                 this.$wins.innerHTML = word.join('');
-             }
-         }
-         if (this.$right.innerHTML.indexOf('_') < 0) {
-             alert('you win!');
-             this.complete = true;
-         }
-     },
-     losses: function (letter) {
-         this.guessed.push(letter);
-         this.$losses.innerHTML += ' ' + letter;
-         this.left--;
-         this.$remaining.innerHTML = this.left;
-         if (this.left < 1) {
-             alert('you lose! ' + this.word);
-             this.complete = true;
-         }
-     }
- };
- game.start();
- document.onkeyup = function (event) {
-     var letter = String.fromCharCode(event.keyCode).toLowerCase();
-     game.guess(letter);
- };
+var game = {
+    guessed: [],
+    left: 10,
+    start: function () {
+        this.complete = false;
+        this.word = bandNames[Math.floor(Math.random() * bandNames.length)];
+        this.$right = document.getElementById('right');
+        this.$numberRight = document.getElementById('numberRight');
+        this.$wrong = document.getElementById('wrong');
+        this.$remain = document.getElementById('remain');
+        this.right.innerHTML = '_'.repeat(this.word.length);
+    },
+    guess: function (letter) {
+        if (this.left > 0 && this.complete != true) {
+            if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
+                this.right(letter);
+            } else {
+                this.wrong(letter);
+            }
+        }
+    },
+    right: function (letter) {
+        for (var i = 0; i < this.word.length; i++) {
+            if (this.word[i] == letter) {
+                var word = this.right.innerHTML.split('');
+                word[i] = letter;
+                this.right.innerHTML = word.join('');
+            }
+        }
+        if (this.right.innerHTML.indexOf('_') < 0) {
+            alert('you win!');
+            this.complete = true;
+        }
+    },
+    wrong: function (letter) {
+        this.guessed.push(letter);
+        this.wrong.innerHTML += ' ' + letter;
+        this.left--;
+        remain.innerHTML = this.left;
+        if (this.left < 1) {
+            alert('you lose! ' + this.word);
+            this.complete = true;
+        }
+    }
+};
+game.start();
+document.onkeyup = function (event) {
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    game.guess(letter);
+};
